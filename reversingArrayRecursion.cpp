ba574fe37ce2a9c69793576065c9871998928c86
Jaswanth Kunjeti <kunjetijassvanthh@gmail.com>
2022-05-06 20:43:47 +0530

    Create reversingArrayRecursion.cpp

---
+++ reversingArrayRecursion.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+void swap(int l, int r, int arr[]){
+	if(l>r){
+		return;
+	}
+	int temp;
+	temp = arr[r];
+	arr[r] = arr[l];
+	arr[l] = temp;
+	swap(l+1,r-1,arr);
+}
+
+void swap2(int l, int arr[], int n){
+	if(l>n-l){
+		return;
+	}
+	int temp;
+	temp = arr[l];
+	arr[l] = arr[n-l-1];
+	arr[n-l-1] = temp;
+	swap2(l+1, arr, n);
+}
+
+int main(){
+	int arr[5] = {1,2,3,4,5};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	swap(0,4,arr);
+	for(int i: arr){
+		cout << i << " ";
+	}
+	cout << "\n";
+	
+	swap2(0,arr, size);
+	for(int i: arr){
+		cout << i << " ";
+	}
+
+}
+
